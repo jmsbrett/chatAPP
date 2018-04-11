1019171a73b6060688dfb6c9ada1c0274a1520a9
James Brett <jmsbrett@gmail.com>
2018-04-11 09:54:03 -0500

    changes

---
+++ server/index.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let app = express();
+
+let http = require('http');
+let server = http.Server(app);
+
+let socketIO = require('socket.io');
+let io = socketIO(server);
+
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('User Connected');
+});
+
+server.listen(port, () => {
+    console.log(`Started on port: ${port}`);
+});
